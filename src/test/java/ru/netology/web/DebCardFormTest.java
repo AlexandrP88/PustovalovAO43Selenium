1b3e193bdbac0bd7e3f907f00a7e9a880eda9493
AlexandrP88 <alexsandr@rtural.ru>
2022-01-07 23:45:05 +0500

    crash CI

--- src/test/java/ru/netology/web/DebCardFormTest.java
+++ src/test/java/ru/netology/web/DebCardFormTest.java
@@ -47,7 +47,7 @@ public class DebCardFormTest {
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
         String expectedText = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals(expectedText, actualText);
     }
 }
